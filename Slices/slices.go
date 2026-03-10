75c633d050a9902f11956020563846e599d9e70e
Nurdin564 <nismailov712@gmail.com>
2026-03-10 10:19:53 +0600

    Max function

--- Slices/slices.go
+++ Slices/slices.go
@@ -41,11 +41,126 @@ func main() {
 	// }
 	// fmt.Println(numbers) // [60 40 50 90 70]
 
+	// a := []int{1,2,3,4,5}
+	// a = append(a, 7, 8, 9)
+	// fmt.Println(a, cap(a))   // cap(10)
+	// a = append(a, 2, 78, 90, 23, 45, 67) 
+	// fmt.Println(a, cap(a))   // cap(20)
 
-	fmt.Println(printMagic([]int{1,2,3,4}))
+
+	// fmt.Println(printMagic([]int{1,2,3,4}))
+
+	// b := []int{34, 56, 67, 78, 89, 90}
+	// sum := SumSlices(a, b)
+	// sum1 := SumSlices1(a, b)
+	// fmt.Println(sum, sum1)
+
+	// fmt.Println("Sum:", sum(1,2,3,4,5))
+	// s := []int{1,2,3,4,5,6,7,8,9}
+	// fmt.Println("Sum:", sum(s...))   // spread operator - передает все содержимое слайса
+
+	// s1 := []int{1,2,3}
+	// s2 := []int{6,9}
+	// s1 = append(s1, s2...)
+	// fmt.Println(s1)
+
+	// q := []int{1,2,3,4,5,6,7,8,9}
+	// fmt.Println(filterEven(q...))
+
+	// slices can be compared only with nil
+	// s1 := []string{"Sup", "nigga"}
+	// s2 := []string{"Sup1", "nigga1"}
+	// fmt.Println(s1 == s2)
+
+	// var s3 []int  // nil
+	// fmt.Println(s3, len(s3), cap(s3))
+
 
 
 }
+
+/*
+Вам необходимо реализовать функцию Max, которая будет находить максимальный элемент в слайсе целых чисел. 
+Функция должна принимать слайс в качестве аргумента и возвращать максимальное значение и возможную ошибку. 
+Если слайс равен nil или пуст, функция должна возвращать ошибку с сообщением slice is nil or empty
+*/
+
+func Max(arr []int) (int, error) {
+	if arr == nil || len(arr) == 0 {
+		return 0, fmt.Errorf("slice is nil or empty")
+	}
+	max := 0
+	for _, v := range arr {
+		if v > max {
+			max = v
+		}		
+	}
+	return max, nil
+}
+
+
+
+/*
+Необходимо реализовать функцию filterEven, которая будет фильтровать четные числа из переданного набора целых чисел. 
+Функция должна принимать переменное количество аргументов (целых чисел) и возвращать слайс, 
+содержащий только четные числа, сохраняя порядок переданных в функцию значений в возвращенном слайсе.
+*/
+func filterEven(numbers ...int) []int {
+	result := []int{}
+	for _, v := range numbers {
+		if v%2 == 0 {
+			result = append(result, v)
+		}
+	}
+	return result
+}
+
+
+func sum(values ...int) int {   // variative parameter - можно передать сколько угодно int
+	result := 0
+	for _, v := range values {
+		result += v
+	}
+	return result
+}
+
+
+/*
+Необходимо написать функцию SumSlices, которая будет выполнять сложение двух слайсов целых чисел. 
+Функция должна принимать два слайса в качестве аргументов и возвращать новый слайс, 
+содержащий суммы соответствующих элементов из входных слайсов.
+Если длины двух слайсов различаются, необходимо игнорировать лишние элементы более длинного слайса. 
+То есть, если один слайс длиннее другого, то при сложении следует учитывать только те элементы, которые находятся в пределах длины более короткого слайса.
+Результирующий слайс должен содержать суммы элементов, расположенных на одинаковых позициях в исходных слайсах.
+*/
+
+func SumSlices(arr1, arr2 []int) []int {
+	minLen := len(arr1)
+	if len(arr2) < minLen {
+		minLen = len(arr2)
+	}
+
+	result := make([]int, minLen)    // range doesnot work with int
+	for i := range result {
+		result[i] = arr1[i] + arr2[i]
+	}
+	return result
+}
+
+func SumSlices1(arr1, arr2 []int) []int {
+	minArr := len(arr1)
+	if minArr > len(arr2) {
+		minArr = len(arr2)
+	}
+
+	result := []int{}
+	for i := 0; i < minArr; i++ {
+		result = append(result, arr1[i] + arr2[i])
+	}
+	return result
+}
+
+
 
 /*
 Напишите функцию printMagic, которая принимает слайс целых чисел и выводит строку, похожую на массив, в консоль. 
