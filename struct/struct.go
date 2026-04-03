29a89ca46a749245191a196a1c94ca58b993c104
Nurdin564 <nismailov712@gmail.com>
2026-04-03 19:20:25 +0600

    cont struct

--- struct/struct.go
+++ struct/struct.go
@@ -68,8 +68,66 @@ func main() {
 	// fmt.Printf("%+v\n", user1)  // {Name:{First:Arthur Last:Celestial} BirthYear:1888}
 
 
+	user := User{
+		ID:    1,
+		Name:  "Иван Петров",
+		Email: "ivan.petrov@example.com",
+		Phone: "+7 999 123-45-67",
+		Address: Address{
+			Street:     "Улица Ленина",
+			City:       "Москва",
+			PostalCode: "101000",
+		},
+		Cart: []CartItem{
+			{
+				Product: Product{
+					ID:          1,
+					Name:        "Ноутбук",
+					Description: "Мощный ноутбук для работы и игр",
+					Price:       59990,
+					Category:    "Электроника",
+					Brand:       "Brand A",
+					Rating:      4.5,
+					Reviews:     120,
+				},
+				Quantity: 1,
+			},
+			{
+				Product: Product{
+					ID:          2,
+					Name:        "Смартфон",
+					Description: "Современный смартфон с отличной камерой",
+					Price:       29990,
+					Category:    "Электроника",
+					Brand:       "Brand B",
+					Rating:      4.7,
+					Reviews:     200,
+				},
+				Quantity: 2,
+			},
+			{
+				Product: Product{
+					ID:          3,
+					Name:        "Наушники",
+					Description: "Беспроводные наушники с шумоподавлением",
+					Price:       7990,
+					Category:    "Аудио",
+					Brand:       "Brand C",
+					Rating:      4.3,
+					Reviews:     80,
+				},
+				Quantity: 1,
+			},
+		},
+	}
 
+	printInfo(user)
+
+
+
+	
 }
+
 
 /*
 Функция printInfo должна выводить следующую информацию, каждое сообщение на новой строке:
